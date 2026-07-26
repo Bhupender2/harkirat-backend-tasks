fa849fa10fa7cdbf7949db494618daf812c17057
Bhupender2 <b.mehtaa2021@gmail.com>
2026-07-26 15:59:35 +0530

    x.y.z this is for version here

--- index.mjs
+++ index.mjs
@@ -4,3 +4,7 @@ console.log(chalk.red("helllo world"));
 
 // --experimental-modules is being used back when the es modules were not stable now  so we dont need them now in the modern nodejs environment
 // internal package are those package that nodejs provide out of the box we dont have to install it
+
+// when i installed express it also downloads some other packages as well . which express was dependent upon
+
+//x.y.z major.minor.patch this is for version
