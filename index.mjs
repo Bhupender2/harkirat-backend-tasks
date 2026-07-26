70d24d14b147c06d6ec135a23e07e84465a4e1c9
Bhupender2 <b.mehtaa2021@gmail.com>
2026-07-26 15:15:59 +0530

    gitignore is being added here

---
+++ index.mjs
@@ -0,0 +1,6 @@
+import chalk from "chalk"; // more modern import syntax
+
+console.log(chalk.red("helllo world"));
+
+// --experimental-modules is being used back when the es modules were not stable now  so we dont need them now in the modern nodejs environment
+// internal package are those package that nodejs provide out of the box we dont have to install it
