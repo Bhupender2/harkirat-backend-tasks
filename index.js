91867393ba6d3d8295ac7e27c1f988dfccb7c4e3
Bhupender2 <b.mehtaa2021@gmail.com>
2026-07-27 15:45:37 +0530

    index.js done here

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs"); //
+
+function main(fileName) {
+  fs.readFile(fileName, "utf-8", function (err, data) {
+    // Hello world
+    let total = 0;
+    for (let i=0; i< data.length  i++){
+      // hello world
+      
+    }
+  });
+}
+
+main();
