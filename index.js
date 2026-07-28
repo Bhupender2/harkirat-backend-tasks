d3a2e9e65f6673446fb66d5caa7968d45a6e0c56
Bhupender2 <b.mehtaa2021@gmail.com>
2026-07-28 17:51:52 +0530

    comments has been added here

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fs = require("fs"); //
 
 function main(fileName) {
   fs.readFile(fileName, "utf-8", function (err, data) {
-    // Hello world
+    // Hello world.
+    // inside this function data the argument contain all the text inside it right
     let total = 0;
     console.log("total : ", data);
     for (let i = 0; i < data.length; i++) {
