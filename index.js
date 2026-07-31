9e4b5f3df0447c3e670e70806d65e3423d3d5df9
Bhupender2 <b.mehtaa2021@gmail.com>
2026-07-31 06:24:14 +0530

    index.js done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs"); // its a built in module to read and write the files
 
 function main(fileName) {
+  console.log(process.argv); // contains all the command i give in the cli here will be reflected
   fs.readFile(fileName, "utf-8", function (err, data) {
     // basically fs.readFile ek pathroute argument leta h second encoding and third ek callback with err and data(the content inside file)
     // Hello world.
@@ -17,6 +18,6 @@ function main(fileName) {
   });
 }
 
-main("a.txt");
+main(process.argv[2]);
 
 // cat is basically a command that reads one or more files and prints their contents to the terminal . it can also concatanate mutiple files into a single output
