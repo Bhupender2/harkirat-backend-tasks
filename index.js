80483c415b71d6db588947028b3b9609e9b0ccae
Bhupender2 <b.mehtaa2021@gmail.com>
2026-07-31 05:58:20 +0530

    index.js done

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const fs = require("fs"); // its a built in module to read and write the files
+// const fs = require("fs"); // its a built in module to read and write the files
+
+// function main(fileName) {
+//   fs.readFile(fileName, "utf-8", function (err, data) {
+//     // Hello world.
+//     // inside this function data the argument contain all the text inside it right
+//     let total = 0;
+//     console.log("total : ", data);
+//     for (let i = 0; i < data.length; i++) {
+//       // hello world
+//       if (data[i] === " ") {
+//         total++;
+//       }
+//     }
+//     console.log("habibi", total + 1);
+//   });
+// }
+
+// main("a.txt");
+
+// // cat is basically a command that reads one or more files and prints their contents to the terminal . it can also concatanate mutiple files into a single output
+const fs = require("fs");
 
 function main(fileName) {
   fs.readFile(fileName, "utf-8", function (err, data) {
-    // Hello world.
-    // inside this function data the argument contain all the text inside it right
+    // hello world
     let total = 0;
-    console.log("total : ", data);
     for (let i = 0; i < data.length; i++) {
-      // hello world
       if (data[i] === " ") {
         total++;
       }
@@ -17,5 +35,3 @@ function main(fileName) {
 }
 
 main("a.txt");
-
-// cat is basically a command that reads one or more files and prints their contents to the terminal . it can also concatanate mutiple files into a single output
